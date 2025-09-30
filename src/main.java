aca96a939842aa4dcd23c2225d5ffe556d63cb8d
Carson Rochon <rochoncarsond@gmail.com>
2025-09-30 17:38:08 -0600

    Setting up for next time i work on this project with TODOs and comments to guide

--- src/main.java
+++ src/main.java
@@ -13,14 +13,16 @@ class Main {
         frame.setSize(900, 600); // w x h
 
         JPanel bottompanel = new JPanel();
-        JButton psbutton = new JButton("Pause");
-        JButton rsbutton = new JButton("Resume");
+        JButton psbutton = new JButton("Pause"); // mPlayer.play();
+        JButton rsbutton = new JButton("Play"); // mPlayer.pause();
 
         JPanel toppanel = new JPanel();
         JButton audiobutton = new JButton("Select Audio");
+        // TODO: Listen for button press -> GrabAudio() -> Media loaded & Media player ready
 
         bottompanel.add(psbutton);
         bottompanel.add(rsbutton);
+
         toppanel.add(audiobutton);
 
         frame.add(bottompanel, BorderLayout.SOUTH); // push button to bottom
@@ -30,7 +32,11 @@ class Main {
         frame.setVisible(true);
     }
 
-    static void GrabAudio(String audiofile) {
+    static void GrabAudio() {
+        // TODO: "Select Audio" pressed -> File explorer window pops up (certain extensions allowed) -> grab file path
 
+        // Media med = new Media(getClass().getResource(filepath).toExternalForm());
+        // MediaPlayer mPlayer = new MediaPlayer(med);
     }
+
 }
