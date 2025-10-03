a4e357400f8edf1b57fe893ce6a7d7e762d717dc
Carson Rochon <rochoncarsond@gmail.com>
2025-10-03 07:17:24 -0600

    Added actionlistener on audiobutton when clicked it calls opendialog() (opens file chooser)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,11 @@ import javafx.scene.media.MediaPlayer;
 
 // Main class
 class Main {
-    // Main driver method
-    public static void main(String[] args)
-    {
+
+    private JButton audiobutton; // using variable in more than one method
+
+
+    public void initUI() { // Can't use main since it's a static method
         // Creating instance of JFrame
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Actually closes the program instead of staying open forever
@@ -18,14 +20,18 @@ class Main {
         JButton psbutton = new JButton("Pause"); // mPlayer.play();
         JButton rsbutton = new JButton("Play"); // mPlayer.pause();
 
+        
         JPanel toppanel = new JPanel();
+
         JButton audiobutton = new JButton("Select Audio");
+        audiobutton.addActionListener(e -> OpenDialog());
         // TODO: Listen for button press -> GrabAudio() -> Media loaded & Media player ready
 
         bottompanel.add(psbutton);
         bottompanel.add(rsbutton);
 
         toppanel.add(audiobutton);
+
 
         frame.add(bottompanel, BorderLayout.SOUTH); // push button to bottom
         frame.add(toppanel, BorderLayout.NORTH);
@@ -34,10 +40,22 @@ class Main {
         frame.setVisible(true);
     }
 
-    static void GrabAudio() {
-        // TODO: "Select Audio" pressed -> File explorer window pops up (certain extensions allowed) -> grab file path.
+    public void OpenDialog() { // button component
         final JFileChooser fc = new JFileChooser();
-        int returnVal = fc.showOpenDialog(fc);
+        int result = fc.showOpenDialog(audiobutton);
+    }
+
+    public static void main(String[] args)
+    
+    {
+        /*JButton audiobutton = new JButton("Select Audio");
+        audiobutton.addActionListener(e -> {
+            final JFileChooser fc = new JFileChooser();
+            int result = fc.showOpenDialog(audiobutton);
+        }); */ // Man oh man, I decided to make a non-static method to reference OpenDialog even though I could've done the comment above -_-
+               // Stil allows me to work in the OpenDialog method which helps with organization
+
+        new Main().initUI();
     }
 
 }
