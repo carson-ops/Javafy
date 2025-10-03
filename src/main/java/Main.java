52b30c40535d7921ce6588fed8cf8c7678094cd8
Carson Rochon <rochoncarsond@gmail.com>
2025-10-03 07:55:35 -0600

    Commit checkpoint. Added label in GUI for clarity on file picked. When file picked, call getselectedfile for the path then use label in GUI to display

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.media.MediaPlayer;
 class Main {
 
     private JButton audiobutton; // using variable in more than one method
+    private JLabel currentsong;
 
 
     public void initUI() { // Can't use main since it's a static method
@@ -16,38 +17,53 @@ class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Actually closes the program instead of staying open forever
         frame.setSize(900, 600); // w x h
 
-        JPanel bottompanel = new JPanel();
+        JPanel bottompanel = new JPanel(); // bottom of screen
         JButton psbutton = new JButton("Pause"); // mPlayer.play();
         JButton rsbutton = new JButton("Play"); // mPlayer.pause();
 
         
-        JPanel toppanel = new JPanel();
-
+        JPanel toppanel = new JPanel(); // top of screen
         JButton audiobutton = new JButton("Select Audio");
+
         audiobutton.addActionListener(e -> OpenDialog());
         // TODO: Listen for button press -> GrabAudio() -> Media loaded & Media player ready
+
+        JPanel midpanel = new JPanel(); // middle of screen
+        currentsong = new JLabel("No Song Picked."); // NOTE: don't redeclare fields, just assign them | stumped me when I called JLabel before current song
 
         bottompanel.add(psbutton);
         bottompanel.add(rsbutton);
 
         toppanel.add(audiobutton);
 
+        midpanel.add(currentsong);
 
         frame.add(bottompanel, BorderLayout.SOUTH); // push button to bottom
         frame.add(toppanel, BorderLayout.NORTH);
+        frame.add(midpanel, BorderLayout.CENTER);
 
-        // making the frame visible
+        // making the frame visible 
         frame.setVisible(true);
     }
 
     public void OpenDialog() { // button component
         final JFileChooser fc = new JFileChooser();
         int result = fc.showOpenDialog(audiobutton);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+
+            if (file == null) {
+                currentsong.setText("No Song Picked! test"); 
+            } else {
+                currentsong.setText("Song Playing: " + file);
+            }
+             
+        }
     }
 
-    public static void main(String[] args)
-    
-    {
+    public static void main(String[] args) {
+
         /*JButton audiobutton = new JButton("Select Audio");
         audiobutton.addActionListener(e -> {
             final JFileChooser fc = new JFileChooser();
